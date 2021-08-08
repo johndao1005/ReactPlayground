a1a6fffb55abac9107c88ea4b370f29ff64b510d
johndao1005 <john.d1729@gmail.com>
2021-08-08 18:42:30 +1200

    creating module with node

--- src/test.js
+++ src/test.js
@@ -1,8 +1,6 @@
 // Create simple function
 const test = function() {
-    return console.log("Hello from test File")
-}
-
-//test()
-
+        return console.log("Hello from test File")
+    }
+    //test()
 module.exports = test // export function test for global use
